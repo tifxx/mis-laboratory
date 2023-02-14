90b055674a4db5f5652f9ead15a9fbd97c96471c
tifxx <94193779+tifxx@users.noreply.github.com>
2023-02-14 15:53:55 +0100

    lab 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart' as geolocator;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:lab3/widgets/calendar.dart';
+import 'package:location/location.dart';
 import 'Model/list_item.dart';
 import 'widgets/new_element.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'widgets/login_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'api_key.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -91,6 +95,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with RestorationMixin {
   @override
   void initState() {
+    locatePosition();
     super.initState();
 
     flutterLocalNotificationsPlugin.initialize(
@@ -105,6 +110,17 @@ class _MyHomePageState extends State<MyHomePage> with RestorationMixin {
   final RestorableInt _index = RestorableInt(0);
   GoogleMapController? _controller;
   List<Marker> markers = [];
+  List<Polyline> polyline = [];
+  List<LatLng> polylineCoordinates = [];
+  late geolocator.Position currentPosition;
+  var geoLocator = geolocator.Geolocator();
+
+  void locatePosition() async {
+    geolocator.Position position =
+        await geolocator.Geolocator.getCurrentPosition(
+            desiredAccuracy: geolocator.LocationAccuracy.high);
+    currentPosition = position;
+  }
 
   final List<ListItem> _elements = [
     // ListItem(
@@ -256,31 +272,84 @@ class _MyHomePageState extends State<MyHomePage> with RestorationMixin {
               IconButton(
                 icon: Icon(Icons.location_on_outlined),
                 onPressed: () {
-                  showModalBottomSheet(
-                      context: context,
-                      isScrollControlled: true,
-                      builder: (_) => SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: 500,
-                          child: GoogleMap(
-                            mapType: MapType.normal,
-                            onMapCreated: (GoogleMapController controller) {
-                              _controller = controller;
-                            },
-                            initialCameraPosition: const CameraPosition(
-                                target: LatLng(42.0041222, 21.4073592),
-                                zoom: 14),
-                            myLocationEnabled: true,
-                            myLocationButtonEnabled: true,
-                            markers:
-                                setMarker(course.latitude, course.longitude),
-                          )));
+                  _onPressedForMap(course);
                 },
               ),
             ],
           ),
         ),
       );
+
+  Widget _widget = CircularProgressIndicator();
+
+  void _onPressedForMap(ListItem course) {
+    polylineCoordinates.clear();
+    getPolyPoints(course.latitude, course.longitude, currentPosition.latitude,
+            currentPosition.longitude)
+        .then((e) {
+      if (polylineCoordinates.isEmpty) {
+        setState(() {
+          _widget = CircularProgressIndicator();
+        });
+      } else {
+        setState(() => MapScreen(course));
+      }
+    });
+  }
+
+  void MapScreen(ListItem course) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        builder: (_) => SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 500,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              onMapCreated: (GoogleMapController controller) {
+                _controller = controller;
+              },
+              initialCameraPosition: const CameraPosition(
+                  target: LatLng(42.0041222, 21.4073592), zoom: 14),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              markers: setMarker(course.latitude, course.longitude),
+              polylines: {
+                Polyline(
+                  polylineId: PolylineId(course.id),
+                  points: polylineCoordinates,
+                  color: Colors.red,
+                  width: 6,
+                ),
+              },
+            )));
+  }
+
+  Future<List<LatLng>?> getPolyPoints(double sourceLat, double sourceLon,
+      double destLat, double destLon) async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleMapsApiKey,
+      PointLatLng(sourceLat, sourceLon),
+      PointLatLng(destLat, destLon),
+      travelMode: TravelMode.driving,
+    );
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+
+      return polylineCoordinates;
+    } else {
+      print(result.errorMessage);
+      return null;
+    }
+  }
 
   Set<Marker> setMarker(double lat, double lon) {
     LatLng point = LatLng(lat, lon);
